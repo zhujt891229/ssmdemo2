4baca8ffd749c2d86e5076d38712dd9c6e540340
zhujt <532151620@qq.com>
2020-10-16 10:55:12 +0800

    初始化ssmdemo2样例

---
+++ src/main/java/com/sgcc/user/dao/UserDao.java
@@ -0,0 +1,17 @@
+package com.sgcc.user.dao;
+
+import com.sgcc.user.pojo.User;
+
+public interface UserDao {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(User record);
+
+    int insertSelective(User record);
+
+    User selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(User record);
+
+    int updateByPrimaryKey(User record);
+}
---
+++ src/main/java/com/sgcc/user/service/UserService.java
@@ -0,0 +1,11 @@
+package com.sgcc.user.service;
+
+import com.sgcc.user.pojo.User;
+
+public interface UserService {
+    //根据id查找
+    public User getUserById(Integer userid);
+
+    //添加一条数据
+    public int insert(User user);
+}
